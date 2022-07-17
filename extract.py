35bcd50f97ede73831384f794b0c472f594fdbb3
Matias Aleksandrowicz <matiasaleksandrowicz@Matiass-MacBook-Pro.local>
2022-07-17 19:29:21 -0300

    Carga de a 20(por las limitaciones de la API)

--- extract.py
+++ extract.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import requests
 import airflow
-from pandas.io.json import json_normalize
 
 # Guardo el url de la api, params y headers
 api_url = 'https://api.yelp.com/v3/businesses/search'
@@ -12,14 +11,17 @@ api_key = "bdGAuRF-OkcKcqikk8RYXv6ii5pAyos8DbWwQhjxkQxeaQA0-RLJtRuMkKKc-d55XeUH6
           "-4bhpFDpMYugPiVVR7rpX5KswXDZqMHxe602Jxgn7UYnYx "
 headers = {"Authorization": "Bearer {}".format(api_key)}
 
-# Guardo el json que me da una api en una variable url_api
-api_get_info = requests.get(api_url, params=params, headers=headers)
-
-# Con pandas leo ese json, con params y headers adecuados
-response = pd.read_json(api_get_info)
+# API requests para conectarme al search engine de Yelp en Buenos Aires
+api_get_info_first20 = requests.get(api_url, params=params, headers=headers)
 
 # Con el method json me quedo solo con la data de la api, excluyo la metadata
-data_clean = response.json()
+data_clean = api_get_info_first20.json()
 
 # Otra vez con pandas, esta vez creo un dataframe con la data recibida y limpia de la api
-datafra = pd.DataFrame(data_clean[""])
+restaurants_buenos_aires_first20 = pd.json_normalize(data_clean["businesses"], sep="_")
+params["offset"] = 20
+api_get_info_next20 = requests.get(api_url, headers=headers, params=params).json()
+restaurants_buenos_aires_next20 = pd.json_normalize(api_get_info_next20["businesses"], sep="_")
+
+restaurants_buenosaires = pd.concat([restaurants_buenos_aires_first20, restaurants_buenos_aires_next20])
+print(restaurants_buenosaires.name)
